64c8bd0f502e4b734f34f9d577f8c9cc323b553f
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-09-29 17:42:27 -0300

    Requisito 2 - completo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalRequired = data.species.find((animals) => animals.name === animal);
+  return animalRequired.residents.every((elements) => elements.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
