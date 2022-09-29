d0ad717eacab116d76b95ac7442b8f77cc4b0565
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-09-29 19:20:38 -0300

    Requisito 4 - completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const a = data.employees.some((employee) => employee.managers.some((ids) => ids === id));
+  return a;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const falseTrue = isManager(managerId);
+  if (falseTrue === true) {
+    const staff = [];
+    const employees = data.employees.filter((employee) => employee.managers.includes(managerId));
+    employees.forEach((person) => staff.push(`${person.firstName} ${person.lastName}`));
+    return staff;
+  }
+  if (falseTrue === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
