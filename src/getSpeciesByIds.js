d8d6f42d066e2b47901b8367aa4bda913b239995
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-09-29 17:30:09 -0300

    Requisito 1 - completo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const array = [];
+  ids.forEach((id) => {
+    array.push(data.species.find((animal) => id === animal.id));
+  });
+  return array;
 }
+
+getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d', '0938aa23-f153-4937-9f88-4858b24d6bce');
 
 module.exports = getSpeciesByIds;
